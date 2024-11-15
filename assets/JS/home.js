d73e4361171c68837d3e33f54cd7991ae22fd4ec
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2024-11-15 03:56:39 -0800

    began working on the logic of displaying the local stroage content via the DOM. The implementation will change a lot in the future as it gets connected to other parts of the codebase.

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -12,3 +12,99 @@ function sortByTime(apptArray){
     /*https://www.geeksforgeeks.org/arrow-functions-in-javascript/ */
     return apptArray.sort((a,b) => a.dayAndTime.dateTime - b.dayAndTime.dateTime);
 }
+
+// TODO: Create button to direct to form page
+const mainContent = document.querySelector('main')
+
+
+// TODO: Create a function that handles the case where there are no appointments
+function noAppointments(){
+    const noApptErr = document.createElement('p');
+    noApptErr.textContent = 'There are no appoinments'
+    mainContent.appendChild(noApptErr);
+}
+
+// TODO: Create a function called `renderAppointments` that renders the list of blog posts if they exist. If not, call the no posts function.
+function renderAppointments(){
+    mainContent.innerHTML = '';
+    const listEl = document.createElement('ul');
+    mainContent.appendChild(listEl);
+
+    let appointments = JSON.parse(localStorage.getItem('appointmentInfo'));
+
+    if(appointments === null){
+        noAppointments();
+    }
+    //if the local storage is not an array (It will be in the future)
+    else{
+        const listItemEl = document.createElement('li');
+        const sectionEl = document.createElement('section');
+        const h1El = document.createElement('h1');
+        const h2El = document.createElement('h2');
+        const pEl = document.createElement('p');
+
+        h1El.textContent = `appointment 1`;
+        h2El.textContent = `${appointments.dayAndTime}`;
+        //pEl.textContent = `${appointments[i].dateTime}`
+
+        mainContent.appendChild(sectionEl);
+
+        sectionEl.appendChild(h1El);
+        sectionEl.appendChild(h2El);
+        sectionEl.appendChild(pEl);
+
+        listItemEl.appendChild(sectionEl);
+        listEl.appendChild(listItemEl);
+        
+        console.log(listEl);
+    }
+    // else{
+    //     console.log("Appointments.length = " + appointments.length);
+    //     //console.log ("appointments = " + JSON.stringify(appointments));
+    //     for(let i = 0; i < appointments.length; i++){
+    //         const listItemEl = document.createElement('li');
+
+    //         const sectionEl = document.createElement('section');
+    //         const h1El = document.createElement('h1');
+    //         const h2El = document.createElement('h2');
+    //         const pEl = document.createElement('p');
+
+    //         h1El.textContent = `appointment number ${appointments[i + 1]}`;
+    //         console.log("i = " + i);
+    //         h2El.textContent = `${appointments[i].dayAndTime}`;
+    //         //pEl.textContent = `${appointments[i].dateTime}`
+
+    //         mainContent.appendChild(sectionEl);
+    //         sectionEl.appendChild(h1El);
+    //         sectionEl.appendChild(h2El);
+    //         sectionEl.appendChild(pEl);
+
+    //         listItemEl.appendChild(sectionEl);
+    //         listEl.appendChild(listItemEl);
+            
+    //         console.log(listEl);
+
+    //         // const articleEl = document.createElement('article');
+    //         // const articleH2El = document.createElement('h2');
+    //         // //const pEl = document.createElement('p');
+    //         // const blockQuoteEl = document.createElement('blockquote');
+
+    //         // articleH2El.textContent = `${appointments[i].title}`;
+    //         // pEl.textContent = `${appointments[i].username}`;
+    //         // blockQuoteEl.textContent = `${appointments[i].content}`;
+
+    //         // articleEl.appendChild(articleH2El);
+    //         // articleEl.appendChild(pEl);
+    //         // articleEl.appendChild(blockQuoteEl);
+
+    //         // listItemEl.appendChild(articleEl);
+    //         // listEl.appendChild(listItemEl);
+            
+    //         // console.log(listEl);
+    //     }
+    // }
+
+}
+
+//call functions
+renderAppointments()
