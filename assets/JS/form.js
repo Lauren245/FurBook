f344079f5dd269b594f52ab2e37717a185a58abe
Tyler Kelly <tylerm.kelly16@gmail.com>
2024-11-14 22:26:07 -0700

    update

--- assets/JS/form.js
+++ assets/JS/form.js
@@ -1,1 +1,9 @@
 /*This file will handle the form logic. */
+// const storage = localStorage.getItem('appointmentInfo')
+
+//     console.log(storage)
+//     for (let i = 0; i < localStorage.getItem('appointmentInfo').length; i++) {
+//         console.log(storage[i])
+//     }
+
+// })
