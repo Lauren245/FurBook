955f74ab565a4cbe5b3e1b9a1bba9b9b2dfbed89
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2024-11-15 22:04:02 -0800

    added code to sort and render the list of appointments as retrieved from local storage. Once the index.html file is complete, some of the createElement functions involved with rendering the data will be replaced by querySelectors.

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -1,66 +1,64 @@
 /*this file will handle tasks associated with pulling an appointment
     from local storage and putting it on the index.html page */
 
+const mainContent = document.querySelector('main');
 
-/*TODO (in future): It may be a good idea to provide a way to filter these dates compared to the current date
-    because appointments in the future may be more relevant*/
 
-function sortByTime(apptArray){
+//gets the contents of the local storage array and returns the array sorted by appointment date and time.
+function getStorageArray(){
+    let appointments = JSON.parse(localStorage.getItem('appointmentInfo'));
     /*This arrow function is used to find the difference between the dates
      The array sort method sorts the array based on the results of the arrow function*/
     /*https://chatgpt.com/share/67324451-5a8c-8012-8c7e-d26b3c1277e8 */
     /*https://www.geeksforgeeks.org/arrow-functions-in-javascript/ */
-    return apptArray.sort((a,b) => a.dayAndTime.dateTime - b.dayAndTime.dateTime);
+
+    //must be parsed into dates because local storage stores data as strings
+    return appointments.sort((a,b) => Date.parse(a.dayAndTime.dateTime) - Date.parse(b.dayAndTime.dateTime));
 }
 
-// TODO: Create button to direct to form page
-const mainContent = document.querySelector('main')
 
-
-// TODO: Create a function that handles the case where there are no appointments
+// Create a function that handles the case where there are no appointments
 function noAppointments(){
     const noApptErr = document.createElement('p');
     noApptErr.textContent = 'There are no appoinments'
     mainContent.appendChild(noApptErr);
 }
 
-function generateElements(){
-    const listItemEl = document.createElement('li');
-
-    const sectionEl = document.createElement('section');
-    const h1El = document.createElement('h1');
-
-    const dateHeadingEl = document.createElement('h2');
-    const dateEl = document.createElement('p');
-
-    const ownerHeadingEl = document.createElement('h2');
-    const ownerNameEl = document.createElement('p');
-    const ownerPhoneEl = document.createElement('p');
-
-    const petHeadingEl = document.createElement('h2');
-    const petNameEl = document.createElement('p');
-    const petBreedEl = document.createElement('p');
-    const petAgeEl = document.createElement('p');
-
-    const serviceHeadingEl = document.createElement('h2')
-    const serviceNameEl = document.createElement('p');
-    const serviceTypeEl = document.createElement('p');
-}
-// TODO: Create a function called `renderAppointments` that renders the list of blog posts if they exist. If not, call the no posts function.
+//Create a function called `renderAppointments` that renders the list of blog posts if they exist. If not, call the no posts function.
 function renderAppointments(){
     mainContent.innerHTML = '';
     const listEl = document.createElement('ul');
     mainContent.appendChild(listEl);
 
-    let appointments = JSON.parse(localStorage.getItem('appointmentInfo'));
+    let appointments = getStorageArray()
 
     if(appointments === null){
         noAppointments();
     }
     else{
         console.log("Appointments.length = " + appointments.length);
-        //console.log ("appointments = " + JSON.stringify(appointments));
         for(let i = 0; i < appointments.length; i++){
+
+            const listItemEl = document.createElement('li');
+
+            const sectionEl = document.createElement('section');
+            const h1El = document.createElement('h1');
+        
+            const dateHeadingEl = document.createElement('h2');
+            const dateEl = document.createElement('p');
+        
+            const ownerHeadingEl = document.createElement('h2');
+            const ownerNameEl = document.createElement('p');
+            const ownerPhoneEl = document.createElement('p');
+        
+            const petHeadingEl = document.createElement('h2');
+            const petNameEl = document.createElement('p');
+            const petBreedEl = document.createElement('p');
+            const petAgeEl = document.createElement('p');
+        
+            const serviceHeadingEl = document.createElement('h2')
+            const serviceNameEl = document.createElement('p');
+            const serviceTypeEl = document.createElement('p');
         
             let apptNumber = i+1;
 
@@ -115,32 +113,18 @@ function renderAppointments(){
             console.log(listEl);
         }
     }
-
 }
 
 //call functions
-renderAppointments()
+renderAppointments();
 
-    //if the local storage is not an array (It will be in the future)
-    // else{
-    //     const listItemEl = document.createElement('li');
-    //     const sectionEl = document.createElement('section');
-    //     const h1El = document.createElement('h1');
-    //     const h2El = document.createElement('h2');
-    //     const pEl = document.createElement('p');
 
-    //     h1El.textContent = `appointment 1`;
-    //     h2El.textContent = `${appointments.dayAndTime.dateTime}`;
-    //     //pEl.textContent = `${appointments[i].dateTime}`
+//Create button to direct to form page
+//!!! NOTE: In the release version this button will be created in index.html and referenced here instead.
+const backButton = document.createElement('button');
+backButton.textContent = "New Appointment Form";
+mainContent.appendChild(backButton);
 
-    //     mainContent.appendChild(sectionEl);
-
-    //     sectionEl.appendChild(h1El);
-    //     sectionEl.appendChild(h2El);
-    //     sectionEl.appendChild(pEl);
-
-    //     listItemEl.appendChild(sectionEl);
-    //     listEl.appendChild(listItemEl);
-        
-    //     console.log(listEl);
-    // }
+backButton.addEventListener('click', function(){
+    location.assign("form.html");
+});
